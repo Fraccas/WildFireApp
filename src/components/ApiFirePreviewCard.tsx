36fd75a95097021cb9f6ac393d1765838faf8d73
Fraccas <daltonward1@yahoo.com>
2019-10-24 23:11:45 -0600

    using inciweb api to pull live fires

---
+++ src/components/ApiFirePreviewCard.tsx
@@ -0,0 +1,64 @@
+import * as React from 'react';
+import { Text, Card, Button, Icon } from 'react-native-elements';
+import { NavigationInjectedProps, withNavigation } from 'react-navigation';
+import { getLocationText } from '../utils/map';
+
+interface Props extends NavigationInjectedProps {
+    apifire: {
+        id: string,
+        title: string,
+        published: string,
+        link: string,
+        lat: number,
+        lon: number,
+        distanceFromUser: number
+    }
+    
+}
+
+interface State {
+    locationText: string,
+}
+
+class ApiFirePreviewCard extends React.Component<Props, State> {
+    constructor(props: Props) {
+        super(props);
+        this.state = {
+          locationText: 'NA'
+        }
+      }
+
+    async componentDidMount() {
+        let locText:string = await getLocationText(this.props.apifire.lat, this.props.apifire.lon) as string;
+        this.setState({locationText: locText});
+
+    }
+
+    render () {   
+        if (this.state.locationText != "NA" && this.props.apifire.title) {
+            const id = 'apifire-' + this.props.apifire.id;
+            return (
+                <Card
+                    title={this.props.apifire.title}
+                    image={{uri: 'https://images.newscientist.com/wp-content/uploads/2018/08/08114243/rexfeatures_9778243j.jpg'}}
+                >
+                    <Text style={{marginBottom: 10}}>{`Distance from you: ${this.props.apifire.distanceFromUser} miles`}</Text>
+                    <Text style={{marginBottom: 10}}>{`Located in ${this.state.locationText}`}</Text>
+                    <Text style={{marginBottom: 10}}>{`${this.props.apifire.published}`}</Text>
+                    <Button
+                        icon={<Icon name='code' color='#ffffff' />}
+                        buttonStyle={{ backgroundColor: '#36454f', borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0, marginTop: 0 }}
+                        title=' View this Fire'
+                        onPress={() => this.props.navigation.navigate('SingleFire', {
+                            itemId: id
+                        })}
+                    />
+                </Card>
+            );
+        } else {
+            return null;  
+        }
+    }
+}
+
+export default withNavigation(ApiFirePreviewCard);
--- src/screens/AllFires.tsx
+++ src/screens/AllFires.tsx
@@ -4,8 +4,22 @@ import { NavigationStackScreenProps } from 'react-navigation-stack';
 import { NavigationScreenOptions, NavigationEvents } from 'react-navigation';
 import { json } from '../utils/api';
 import FirePreviewCard from '../components/FirePreviewCard';
-import { getDistance } from 'geolib';
+import ApiFirePreviewCard from '../components/ApiFirePreviewCard';
 
+
+// external libs
+import { getDistance } from 'geolib';
+const parseString = require('react-native-xml2js').parseString;
+
+interface apiFire {
+  id: string,
+  title: string,
+  published: string,
+  link: string,
+  lat: number,
+  lon: number,
+  distanceFromUser: number
+}[]
 interface IHomeProps extends NavigationStackScreenProps { }
 interface IHomeState {
   fires: {
@@ -18,7 +32,16 @@ interface IHomeState {
     _created: Date,
     distanceFromUser: number
   }[],
-  users: Array<string>
+  users: Array<string>,
+  apiFires: {
+    id: string,
+    title: string,
+    published: string,
+    link: string,
+    lat: number,
+    lon: number,
+    distanceFromUser: number
+  }[]
 }
 
 let myLat: number;
@@ -33,10 +56,17 @@ export default class AllFires extends React.Component<IHomeProps, IHomeState> {
     super(props);
     this.state = {
       fires: [],
-      users: []
+      users: [],
+      apiFires: []
     }
-    this._getFires();
-    navigator.geolocation.getCurrentPosition(this.getPosition); // set gps coords
+  }
+
+  componentDidMount() {
+    //this._getFires();
+    this._getApiFires();
+
+    // set current gps coords from phone on load
+    navigator.geolocation.getCurrentPosition(this.getPosition);
   }
 
   // sets gps coords
@@ -45,63 +75,129 @@ export default class AllFires extends React.Component<IHomeProps, IHomeState> {
     myLon = position.coords.longitude;
   }
 
-  async _getFires() {
+  async _getApiFires() {
     try {
-      let fires = await json('https://report-wildfire-app.herokuapp.com/api/fires'); 
+      fetch('https://inciweb.nwcg.gov/feeds/rss/incidents/') // get xml from api
+        .then(response => response.text())
+        .then((responseText) => {
+          let apifires:any;
+          parseString(responseText, function (err: any, result: any) {
+            if (result) {
+              let jsonFire: any = JSON.stringify(result); // convert xml to json text
+              jsonFire = JSON.parse(jsonFire); // convert json text to json obj
+              apifires = jsonFire['rss']['channel'][0]['item']; // array of all fires
+            }
+          });
 
-      fires.forEach(async function (fire: any) {     
-          const dist = getDistance(
-            { latitude: fire.lat, longitude: fire.lon },
-            { latitude: myLat, longitude: myLon }
-          );
-          // save dist in miles
-          fire.distanceFromUser = Math.round((dist*0.000621) * 100) / 100;
-      });
+          // set lat/long object properties and get distance from user
+          // if (apifires.length > 0) {
+          //   apifires.forEach(function (fire: any) {
+              // fire.lat = parseFloat(fire['geo:lat'][0]);
+              // fire.lon = parseFloat(fire['geo:lon'][0]);
+              // let lat = parseFloat(fire['geo:lat'][0]); 
+              // let lon = parseFloat(fire['geo:lon'][0]);
 
-      this.setState({fires})
-
-      // set author names from db
-      this._setUsers();
-
+              // const dist = getDistance(
+              //   { latitude: lat, longitude: lon },
+              //   { latitude: myLat, longitude: myLon }
+              // );
+              // // save dist in miles
+              // fire.distanceFromUser = Math.round((dist*0.000621) * 100) / 100;
+          //   });
+          // }    
+          this.setState({ apiFires: apifires });
+        })
+        .catch((err) => {
+          console.log('Error fetching the feed: ', err)
+        })
     } catch (e) {
-      console.log(e);
-      Alert.alert("Error on front end api request!");
+      throw e;
     }
   }
 
-    async _setUsers() { 
-      try {
-        let users: Array<string> = [];
-        for (let fire of this.state.fires) {
-          let u = await json('https://report-wildfire-app.herokuapp.com/api/users/' + fire.userid); 
-          users.push(u[0]['name']);
-        }
-        this.setState({users});
-    } catch (e) {
-      console.log(e); 
-    }
-  }
+  // async _getFires() {
+  //   try {
+  //     let fires = await json('https://report-wildfire-app.herokuapp.com/api/fires'); 
 
-  renderFires() {
-    let count = 0;
-    return this.state.fires.map((fire, index) => { 
-      if (this.state.users[index]) {
-        return <FirePreviewCard key={fire.id} fire={fire} authorname={this.state.users[index]} />
-      }
+  //     fires.forEach(async function (fire: any) {     
+
+  //         const dist = getDistance(
+  //           { latitude: fire.lat, longitude: fire.lon },
+  //           { latitude: myLat, longitude: myLon }
+  //         );
+  //         // save dist in miles
+  //         fire.distanceFromUser = Math.round((dist*0.000621) * 100) / 100;
+  //     });
+
+  //     this.setState({fires})
+
+  //     // set author names from db
+  //     this._setUsers();
+
+  //   } catch (e) {
+  //     console.log(e);
+  //     Alert.alert("Error on front end api request!");
+  //   }
+  // }
+
+  //   async _setUsers() { 
+  //     try {
+  //       let users: Array<string> = [];
+  //       for (let fire of this.state.fires) {
+  //         let u = await json('https://report-wildfire-app.herokuapp.com/api/users/' + fire.userid); 
+  //         users.push(u[0]['name']);
+  //       }
+  //       this.setState({users});
+  //   } catch (e) {
+  //     console.log(e); 
+  //   }
+  // }
+
+  // renderFires() {
+  //   return this.state.fires.map((fire, index) => { 
+  //     if (this.state.users[index]) {
+  //       return <FirePreviewCard key={fire.id} fire={fire} authorname={this.state.users[index]} />
+  //     }
+  //   });
+  // }
+
+  renderApiFires() {
+    return this.state.apiFires.map((apifire, index) => {
+      apifire.id = 'api-fire' + index;
+      return <ApiFirePreviewCard key={apifire.id} apifire={apifire} />
     });
   }
 
-  render () { 
-    return (
-      <View style={styles.container}>
-        <NavigationEvents onDidFocus={() => this._getFires()} />
-        <Text style={styles.text}>Current Fires Near You</Text>
-          <ScrollView style={{width: '90%'}}>
-            {this.renderFires()}
+  render() {
+    // if (this.state.fires) {
+    //   return (
+    //     <View style={styles.container}>
+    //       <NavigationEvents onDidFocus={() => this._getFires()} />
+    //       <Text style={styles.text}>{this.state.fires.length} Fires Near You</Text>
+    //         <ScrollView style={{width: '90%'}}>
+    //           {this.renderFires()}
+    //         </ScrollView>
+
+    //     </View>
+    //   );
+    // } else {
+    //   return (<Text style={styles.text}>Loading fire data...</Text>);
+    // }
+
+    if (this.state.apiFires.length > 0) {
+      return (
+        <View style={styles.container}>
+          { <NavigationEvents onDidFocus={() => this._getApiFires()} /> } 
+          <Text style={styles.text}>{this.state.apiFires.length} Fires Found</Text>
+          <ScrollView style={{ width: '90%' }}>
+            {this.renderApiFires()}
           </ScrollView>
-        
-      </View>
-    );
+
+        </View>
+      );
+    } else {
+      return (<Text style={styles.text}>Loading fire data...</Text>);
+    }
   }
 }
 
