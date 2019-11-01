54b098c2f53e1b7956d44d424838df7b50c11d61
Fraccas <daltonward1@yahoo.com>
2019-10-31 22:03:13 -0600

    Jest unit testing implemented

---
+++ src/utils/unit.test.ts
@@ -0,0 +1,11 @@
+// jest unit testing
+let test = 'test';
+it('Unit Test', () => {
+    expect(test).toBe('test');
+});
+
+
+
+export const unitTest = () => {
+    return 'this unit testing with Jest';
+}
